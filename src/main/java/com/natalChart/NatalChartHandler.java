04b3d065bbe42b7872557b5d97fab61a20b3e5b9
Oleg Osadchy <oleg_osadchy@yahoo.com>
2021-11-25 20:04:07 +0000

    change pom.xml

--- src/main/java/com/natalChart/NatalChartHandler.java
+++ src/main/java/com/natalChart/NatalChartHandler.java
@@ -71,13 +71,20 @@ public class NatalChartHandler implements RequestHandler<Object, NatalChartRespo
        List<Double> cusps = Arrays.asList(265.6850555442075D, 307.6441825689919D, 353.38796689506074D,
                26.86890880306794D, 50.191811553503044D, 68.57049261566578D, 85.6850555442075D, 127.64418256899188D,
                173.3879668950608D, 206.8689088030679D, 230.19181155350307D, 248.5704926156658D);
-       Planets thePlanets = (new Planets()).setMoon(Arrays.asList(64.25288092281401D))
-               .setVenus(Arrays.asList(273.7830306594277D)).setJupiter(Arrays.asList(194.01740070357724D))
-               .setNNode(Arrays.asList(158.69506558607495D)).setMars(Arrays.asList(304.51132244447103D))
-               .setLilith(Arrays.asList(229.84586450839743D)).setSaturn(Arrays.asList(255.93879297657875D, -0.2D))
-               .setChiron(Arrays.asList(350.7902705629371D)).setUranus(Arrays.asList(21.33233540624847D))
-               .setSun(Arrays.asList(233.42635496859216D)).setMercury(Arrays.asList(244.23481860676145D))
-               .setNeptune(Arrays.asList(339.24756865409074D)).setPluto(Arrays.asList(285.5359664922833D));
+       Planets thePlanets = (new Planets())
+                .setMoon(Arrays.asList(64.25288092281401D))
+               .setVenus(Arrays.asList(273.7830306594277D)).
+               setJupiter(Arrays.asList(194.01740070357724D))
+               .setNNode(Arrays.asList(158.69506558607495D)).
+               setMars(Arrays.asList(304.51132244447103D))
+               .setLilith(Arrays.asList(229.84586450839743D)).
+               setSaturn(Arrays.asList(255.93879297657875D, -0.2D))
+               .setChiron(Arrays.asList(350.7902705629371D)).
+               setUranus(Arrays.asList(21.33233540624847D))
+               .setSun(Arrays.asList(233.42635496859216D)).
+               setMercury(Arrays.asList(244.23481860676145D))
+               .setNeptune(Arrays.asList(339.24756865409074D)).
+               setPluto(Arrays.asList(285.5359664922833D));
        NatalChartResponse response = (new NatalChartResponse()).setCusps(cusps).setPlanets(thePlanets);
        logger.log("Response: " + GSON.toJson(response));
        logger.log("Response TYPE: " + response.getClass().toString());
