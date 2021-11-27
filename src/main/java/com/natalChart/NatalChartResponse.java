019c8a52bb72b00b71fb10d3192a9931ad2a784b
Oleg Osadchy <oleg_osadchy@yahoo.com>
2021-11-27 19:48:12 +0000

    add toString() for NatalChartResponse

--- src/main/java/com/natalChart/NatalChartResponse.java
+++ src/main/java/com/natalChart/NatalChartResponse.java
@@ -1,31 +1,78 @@
 package com.natalChart;
 
-import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Generated;
 
 public class NatalChartResponse {
-   private Planets planets;
-   private List<Double> cusps = new ArrayList();
-   private String error;
 
-   public Planets getPlanets() {
-      return this.planets;
-   }
+private Planets planets;
+private List<Double> cusps = null;
+private String error;
 
-   public NatalChartResponse setPlanets(Planets planets) {
-      this.planets = planets;
-      return this;
-   }
+/**
+* No args constructor for use in serialization
+*
+*/
+public NatalChartResponse() {
+}
 
-   public List<Double> getCusps() {
-      return this.cusps;
-   }
+/**
+*
+* @param planets
+* @param cusps
+*/
+public NatalChartResponse(Planets planets, List<Double> cusps) {
+super();
+this.planets = planets;
+this.cusps = cusps;
+}
 
-   public NatalChartResponse setCusps(List<Double> cusps) {
-      this.cusps = cusps;
-      return this;
-   }
-   
+public Planets getPlanets() {
+return planets;
+}
+
+public void setPlanets(Planets planets) {
+this.planets = planets;
+}
+
+public NatalChartResponse withPlanets(Planets planets) {
+this.planets = planets;
+return this;
+}
+
+public List<Double> getCusps() {
+return cusps;
+}
+
+public void setCusps(List<Double> cusps) {
+this.cusps = cusps;
+}
+
+public NatalChartResponse withCusps(List<Double> cusps) {
+this.cusps = cusps;
+return this;
+}
+
+@Override
+public String toString() {
+StringBuilder sb = new StringBuilder();
+sb.append(NatalChartResponse.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+sb.append("planets");
+sb.append('=');
+sb.append(((this.planets == null)?"<null>":this.planets));
+sb.append(',');
+sb.append("cusps");
+sb.append('=');
+sb.append(((this.cusps == null)?"<null>":this.cusps));
+sb.append(',');
+if (sb.charAt((sb.length()- 1)) == ',') {
+sb.setCharAt((sb.length()- 1), ']');
+} else {
+sb.append(']');
+}
+return sb.toString();
+}
+
    public String getError() {
       return this.error;
    }
@@ -34,4 +81,5 @@ public class NatalChartResponse {
       this.error = error;
       return this;
    }
+
 }
--- src/main/java/com/natalChart/Planets.java
+++ src/main/java/com/natalChart/Planets.java
@@ -1,137 +1,308 @@
 package com.natalChart;
 
-import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Generated;
 
 public class Planets {
-   private List<Double> moon = new ArrayList();
-   private List<Double> venus = new ArrayList();
-   private List<Double> jupiter = new ArrayList();
-   private List<Double> nNode = new ArrayList();
-   private List<Double> mars = new ArrayList();
-   private List<Double> lilith = new ArrayList();
-   private List<Double> saturn = new ArrayList();
-   private List<Double> chiron = new ArrayList();
-   private List<Double> uranus = new ArrayList();
-   private List<Double> sun = new ArrayList();
-   private List<Double> mercury = new ArrayList();
-   private List<Double> neptune = new ArrayList();
-   private List<Double> pluto = new ArrayList();
 
-   public List<Double> getMoon() {
-      return this.moon;
-   }
+ 
+private List<Double> moon = null;
+ 
+private List<Double> venus = null;
+ 
+private List<Double> jupiter = null;
+ 
+private List<Double> nNode = null;
+ 
+private List<Double> mars = null;
+ 
+private List<Double> lilith = null;
+ 
+private List<Double> saturn = null;
+ 
+private List<Double> chiron = null;
+ 
+private List<Double> uranus = null;
+ 
+private List<Double> sun = null;
+ 
+private List<Double> mercury = null;
+ 
+private List<Double> neptune = null;
+ 
+private List<Double> pluto = null;
 
-   public Planets setMoon(List<Double> moon) {
-      this.moon = moon;
-      return this;
-   }
+/**
+* No args constructor for use in serialization
+*
+*/
+public Planets() {
+}
 
-   public List<Double> getVenus() {
-      return this.venus;
-   }
+/**
+*
+* @param saturn
+* @param nNode
+* @param mars
+* @param mercury
+* @param pluto
+* @param jupiter
+* @param lilith
+* @param sun
+* @param moon
+* @param venus
+* @param uranus
+* @param neptune
+* @param chiron
+*/
+public Planets(List<Double> moon, List<Double> venus, List<Double> jupiter, List<Double> nNode, List<Double> mars, List<Double> lilith, List<Double> saturn, List<Double> chiron, List<Double> uranus, List<Double> sun, List<Double> mercury, List<Double> neptune, List<Double> pluto) {
+super();
+this.moon = moon;
+this.venus = venus;
+this.jupiter = jupiter;
+this.nNode = nNode;
+this.mars = mars;
+this.lilith = lilith;
+this.saturn = saturn;
+this.chiron = chiron;
+this.uranus = uranus;
+this.sun = sun;
+this.mercury = mercury;
+this.neptune = neptune;
+this.pluto = pluto;
+}
 
-   public Planets setVenus(List<Double> venus) {
-      this.venus = venus;
-      return this;
-   }
+public List<Double> getMoon() {
+return moon;
+}
 
-   public List<Double> getJupiter() {
-      return this.jupiter;
-   }
+public void setMoon(List<Double> moon) {
+this.moon = moon;
+}
 
-   public Planets setJupiter(List<Double> jupiter) {
-      this.jupiter = jupiter;
-      return this;
-   }
+public Planets withMoon(List<Double> moon) {
+this.moon = moon;
+return this;
+}
 
-   public List<Double> getNNode() {
-      return this.nNode;
-   }
+public List<Double> getVenus() {
+return venus;
+}
 
-   public Planets setNNode(List<Double> nNode) {
-      this.nNode = nNode;
-      return this;
-   }
+public void setVenus(List<Double> venus) {
+this.venus = venus;
+}
 
-   public List<Double> getMars() {
-      return this.mars;
-   }
+public Planets withVenus(List<Double> venus) {
+this.venus = venus;
+return this;
+}
 
-   public Planets setMars(List<Double> mars) {
-      this.mars = mars;
-      return this;
-   }
+public List<Double> getJupiter() {
+return jupiter;
+}
 
-   public List<Double> getLilith() {
-      return this.lilith;
-   }
+public void setJupiter(List<Double> jupiter) {
+this.jupiter = jupiter;
+}
 
-   public Planets setLilith(List<Double> lilith) {
-      this.lilith = lilith;
-      return this;
-   }
+public Planets withJupiter(List<Double> jupiter) {
+this.jupiter = jupiter;
+return this;
+}
 
-   public List<Double> getSaturn() {
-      return this.saturn;
-   }
+public List<Double> getnNode() {
+return nNode;
+}
 
-   public Planets setSaturn(List<Double> saturn) {
-      this.saturn = saturn;
-      return this;
-   }
+public void setnNode(List<Double> nNode) {
+this.nNode = nNode;
+}
 
-   public List<Double> getChiron() {
-      return this.chiron;
-   }
+public Planets withnNode(List<Double> nNode) {
+this.nNode = nNode;
+return this;
+}
 
-   public Planets setChiron(List<Double> chiron) {
-      this.chiron = chiron;
-      return this;
-   }
+public List<Double> getMars() {
+return mars;
+}
 
-   public List<Double> getUranus() {
-      return this.uranus;
-   }
+public void setMars(List<Double> mars) {
+this.mars = mars;
+}
 
-   public Planets setUranus(List<Double> uranus) {
-      this.uranus = uranus;
-      return this;
-   }
+public Planets withMars(List<Double> mars) {
+this.mars = mars;
+return this;
+}
 
-   public List<Double> getSun() {
-      return this.sun;
-   }
+public List<Double> getLilith() {
+return lilith;
+}
 
-   public Planets setSun(List<Double> sun) {
-      this.sun = sun;
-      return this;
-   }
+public void setLilith(List<Double> lilith) {
+this.lilith = lilith;
+}
 
-   public List<Double> getMercury() {
-      return this.mercury;
-   }
+public Planets withLilith(List<Double> lilith) {
+this.lilith = lilith;
+return this;
+}
 
-   public Planets setMercury(List<Double> mercury) {
-      this.mercury = mercury;
-      return this;
-   }
+public List<Double> getSaturn() {
+return saturn;
+}
 
-   public List<Double> getNeptune() {
-      return this.neptune;
-   }
+public void setSaturn(List<Double> saturn) {
+this.saturn = saturn;
+}
 
-   public Planets setNeptune(List<Double> neptune) {
-      this.neptune = neptune;
-      return this;
-   }
+public Planets withSaturn(List<Double> saturn) {
+this.saturn = saturn;
+return this;
+}
 
-   public List<Double> getPluto() {
-      return this.pluto;
-   }
+public List<Double> getChiron() {
+return chiron;
+}
 
-   public Planets setPluto(List<Double> pluto) {
-      this.pluto = pluto;
-      return this;
-   }
+public void setChiron(List<Double> chiron) {
+this.chiron = chiron;
+}
+
+public Planets withChiron(List<Double> chiron) {
+this.chiron = chiron;
+return this;
+}
+
+public List<Double> getUranus() {
+return uranus;
+}
+
+public void setUranus(List<Double> uranus) {
+this.uranus = uranus;
+}
+
+public Planets withUranus(List<Double> uranus) {
+this.uranus = uranus;
+return this;
+}
+
+public List<Double> getSun() {
+return sun;
+}
+
+public void setSun(List<Double> sun) {
+this.sun = sun;
+}
+
+public Planets withSun(List<Double> sun) {
+this.sun = sun;
+return this;
+}
+
+public List<Double> getMercury() {
+return mercury;
+}
+
+public void setMercury(List<Double> mercury) {
+this.mercury = mercury;
+}
+
+public Planets withMercury(List<Double> mercury) {
+this.mercury = mercury;
+return this;
+}
+
+public List<Double> getNeptune() {
+return neptune;
+}
+
+public void setNeptune(List<Double> neptune) {
+this.neptune = neptune;
+}
+
+public Planets withNeptune(List<Double> neptune) {
+this.neptune = neptune;
+return this;
+}
+
+public List<Double> getPluto() {
+return pluto;
+}
+
+public void setPluto(List<Double> pluto) {
+this.pluto = pluto;
+}
+
+public Planets withPluto(List<Double> pluto) {
+this.pluto = pluto;
+return this;
+}
+
+@Override
+public String toString() {
+StringBuilder sb = new StringBuilder();
+sb.append(Planets.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+sb.append("moon");
+sb.append('=');
+sb.append(((this.moon == null)?"<null>":this.moon));
+sb.append(',');
+sb.append("venus");
+sb.append('=');
+sb.append(((this.venus == null)?"<null>":this.venus));
+sb.append(',');
+sb.append("jupiter");
+sb.append('=');
+sb.append(((this.jupiter == null)?"<null>":this.jupiter));
+sb.append(',');
+sb.append("nNode");
+sb.append('=');
+sb.append(((this.nNode == null)?"<null>":this.nNode));
+sb.append(',');
+sb.append("mars");
+sb.append('=');
+sb.append(((this.mars == null)?"<null>":this.mars));
+sb.append(',');
+sb.append("lilith");
+sb.append('=');
+sb.append(((this.lilith == null)?"<null>":this.lilith));
+sb.append(',');
+sb.append("saturn");
+sb.append('=');
+sb.append(((this.saturn == null)?"<null>":this.saturn));
+sb.append(',');
+sb.append("chiron");
+sb.append('=');
+sb.append(((this.chiron == null)?"<null>":this.chiron));
+sb.append(',');
+sb.append("uranus");
+sb.append('=');
+sb.append(((this.uranus == null)?"<null>":this.uranus));
+sb.append(',');
+sb.append("sun");
+sb.append('=');
+sb.append(((this.sun == null)?"<null>":this.sun));
+sb.append(',');
+sb.append("mercury");
+sb.append('=');
+sb.append(((this.mercury == null)?"<null>":this.mercury));
+sb.append(',');
+sb.append("neptune");
+sb.append('=');
+sb.append(((this.neptune == null)?"<null>":this.neptune));
+sb.append(',');
+sb.append("pluto");
+sb.append('=');
+sb.append(((this.pluto == null)?"<null>":this.pluto));
+sb.append(',');
+if (sb.charAt((sb.length()- 1)) == ',') {
+sb.setCharAt((sb.length()- 1), ']');
+} else {
+sb.append(']');
+}
+return sb.toString();
+}
+
 }
